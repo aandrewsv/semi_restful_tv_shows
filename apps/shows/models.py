1ea739d1b399d9d8b7c63ba35896ae75d18733ef
Agustin <agustin.andrews@gmail.com>
2021-03-14 13:55:24 -0300

    added validations

--- apps/shows/models.py
+++ apps/shows/models.py
@@ -1,6 +1,23 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import *
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Show title must have at least 2 characters"
+        if len(postData['network']) < 3:
+            errors['network'] = "Show network must have at least 3 characters"
+        if len(postData['description']) < 1:
+            pass
+        elif len(postData['description']) > 1 and len(postData['description']) < 10:
+            errors['description'] = "If present, show description must have at least 10 characters"
+        if datetime.strptime(postData['release_date'], "%Y-%m-%d") > datetime.now():
+            errors['release_date'] = "Show release date must be in the past"
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.TextField()
@@ -8,3 +25,6 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = ShowManager()
+
--- apps/shows/views.py
+++ apps/shows/views.py
@@ -1,5 +1,5 @@
-from django.http.response import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponse
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -8,7 +8,7 @@ def index(request):
 
 def shows(request):
     context = {
-        "all_shows": Show.objects.all()
+        "all_shows": Show.objects.all().order_by("id")
     }
     return render(request, 'shows.html', context)
 
@@ -16,16 +16,29 @@ def new(request):
     return render(request, 'newshow.html')
 
 def newshow(request):
-    Show.objects.create(
-    title=f"{request.POST['title']}",
-    network=f"{request.POST['network']}",
-    release_date=f"{request.POST['release_date']}", 
-    description=f"{request.POST['description']}")
-    return redirect('/shows')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        context = {
+            'show_title': request.POST['title'],
+            'show_network': request.POST['network'],
+            'show_release_date': request.POST['release_date'],
+            'show_description': request.POST['description']
+        }
+        return render(request, 'newshow.html', context)
+    else:
+        Show.objects.create(
+        title=f"{request.POST['title']}",
+        network=f"{request.POST['network']}",
+        release_date=f"{request.POST['release_date']}", 
+        description=f"{request.POST['description']}")
+        messages.success(request, "Show succesfully created")
+        return redirect('/shows')
 
 def displayshow(request, idn):
     context = {
-        'this_show': Show.objects.get(id=idn)
+        'this_show': Show.objects.get(id=idn) 
     }
     return render(request, 'displayshow.html', context)
 
@@ -36,13 +49,19 @@ def editshow(request, idn):
     return render(request, 'editshow.html', context)
 
 def update(request, idn):
-    this_show = Show.objects.get(id=idn)
-    this_show.title = request.POST['title']
-    this_show.network = request.POST['network']
-    this_show.release_date = request.POST['release_date']
-    this_show.description = request.POST['description']
-    this_show.save()
-    return redirect(f'/shows/{idn}')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{idn}/edit')
+    else:
+        this_show = Show.objects.get(id=idn)
+        this_show.title = request.POST['title']
+        this_show.network = request.POST['network']
+        this_show.release_date = request.POST['release_date']
+        this_show.description = request.POST['description']
+        this_show.save()
+        return redirect(f'/shows/{idn}')
 
 def destroy(request, idn):
     this_show = Show.objects.get(id=idn)
